e267d517fb8bba345c96101cba7cb8a0b785682b
baekjung <zvgandam@gmail.com>
2019-08-13 12:21:35 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ fav }) {
+  return <h1>I Like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgyopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
